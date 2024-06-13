b7464e78d418971a10b95049767e40a9fe4d2363
Asaf Marom <asafmarom89@gmail.com>
2024-06-13 17:23:26 +0300

    second

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-300 text-center">Agam CRM</h1>
+      <h2>hello</h2>
+    </div>
+  );
+}
